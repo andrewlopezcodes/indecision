9ec9d54ee96d3b849d1703d8a2d7deda9a91c4d7
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-23 12:09:18 -0400

    created Components file, created components AddOption.js & Option.js

--- src/person.js
+++
@@ -1,9 +0,0 @@
-import {isAdult, canDrink} from './utils.js'
-console.log(isAdult(45))
-console.log(canDrink(23))
-
-console.log('person.js is running too!')
-
-const isSenior =(age)=> age >=65
-
-export default isSenior;
