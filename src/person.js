b5086fe1431be613957c04ddac218ca690d51495
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-23 11:37:36 -0400

    installed react react-dom

---
+++ src/person.js
@@ -0,0 +1,9 @@
+import {isAdult, canDrink} from './utils.js'
+console.log(isAdult(45))
+console.log(canDrink(23))
+
+console.log('person.js is running too!')
+
+const isSenior =(age)=> age >=65
+
+export default isSenior;
