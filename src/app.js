1672941c519272dcebf4bd8f754f72819c414afa
andrewlopezcodes <andrewlopezcodes@gmail.com>
2018-08-23 11:49:41 -0400

    created .babelrc updated webpack.config.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const template  = React.createElement('p', {}, 'testing');
+const template  = <p>This is jsx from webpack</p>;
 ReactDOM.render(template, document.getElementById('app'));
